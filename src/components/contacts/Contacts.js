f2b1a5ce869e33a6976d976e0bb2322deed9bab4
Shashwat Shukla <shuklashashwat2003@gmail.com>
2023-01-14 18:08:50 +0530

    bulk actions stuff

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -42,6 +42,11 @@ const Contacts = () => {
     setUpdateContacts(cid);
     setSelectedContacts([]);
   };
+  const handleBulkActions = (e) => {
+    if (e.target.value == "delete") {
+      showDeleteWarningHandler(selectedContacts);
+    }
+  };
 
   const handleSelectAll = () => {
     if (selectedContacts.length == dataLength) {
@@ -97,18 +102,6 @@ const Contacts = () => {
   return (
     <div className={styles.contactWapper}>
       <div className={styles.contacts}>
-        {selectedContacts.length > 0 ? (
-          <button
-            className={styles.multideletebutton}
-            onClick={() => {
-              showDeleteWarningHandler(selectedContacts);
-            }}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z" />
-            </svg>
-          </button>
-        ) : null}
         {loadingStatus ? (
           <Loader />
         ) : (
@@ -169,6 +162,20 @@ const Contacts = () => {
             </label>
           </div>
         </div>
+        {selectedContacts.length > 0 ? (
+          <div className={styles.bulkactions}>
+            <select
+              className={styles.bulkactions_content}
+              onChange={(e) => {
+                handleBulkActions(e);
+              }}
+            >
+              <option value="">Bulk Actions</option>
+              <option value="delete">Delete</option>
+              <option value="edit">Edit</option>
+            </select>
+          </div>
+        ) : null}
         <div className={styles.driver}>
           <div
             className={`${styles.previous} ${styles.btn}`}
