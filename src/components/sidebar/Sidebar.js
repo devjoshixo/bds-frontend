e586c4ef8b74586f05078d226c31c7271283e463
Shashwat Shukla <shuklashashwat2003@gmail.com>
2023-01-17 12:38:06 +0530

    ...

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import SidebarOptions from "./sidebar-options/SidebarOptions";
 import SidebarHeader from "./sidebarheader/SidebarHeader";
 import styles from "./Sidebar.module.scss";
+import { MdLabelImportant } from "react-icons/md";
 
 const Sidebar = (props) => {
   if (props.selectedAPI === "Dashboard") {
@@ -109,6 +109,14 @@ const Sidebar = (props) => {
           >
             <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"></path>
           </svg>
+        </SidebarOptions>
+        <SidebarOptions
+          name="Custom Fields"
+          closeSidebar={props.closeSidebar}
+          sidebarStatus={props.sidebarStatus}
+          menuToggleHandler={props.menuToggleHandler}
+        >
+          <MdLabelImportant />
         </SidebarOptions>
         <SidebarOptions
           name="Template"
@@ -220,6 +228,14 @@ const Sidebar = (props) => {
           >
             <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"></path>
           </svg>
+        </SidebarOptions>
+        <SidebarOptions
+          name="Custom Fields"
+          closeSidebar={props.closeSidebar}
+          sidebarStatus={props.sidebarStatus}
+          menuToggleHandler={props.menuToggleHandler}
+        >
+          <MdLabelImportant />
         </SidebarOptions>
         <SidebarOptions
           name="Template"
