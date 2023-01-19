319d6d82cd0fac6816c1f5ba775a20d7bed455c3
Shashwat Shukla <shuklashashwat2003@gmail.com>
2023-01-19 15:13:36 +0530

    jr made add contact partially function (missing custom feild stuff)

---
+++ src/API calls/addContact.js
@@ -0,0 +1,11 @@
+const addContact = (contact) => {
+  fetch("http://localhost:5000/contacts/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(contact),
+  });
+};
+
+export default addContact;
